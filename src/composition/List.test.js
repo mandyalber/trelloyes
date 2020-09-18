f54683903bfbaf7bd91e855d478c097bb9086a9a
Mandy Alber <mandy.alber@gmail.com>
2020-09-18 16:35:56 -0700

    fixed List-js test file

--- src/composition/List.test.js
+++ src/composition/List.test.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
-import Card from './Card';
 import renderer from 'react-test-renderer';
 
 describe('List component', () => {
     //smoke test
     it('renders without crashing', () => {
         const section = document.createElement('section')
-        const props = [{title: 'title', content:'content'}];
-
         ReactDOM.render(
-            <List 
-                header = "header" 
-                cards = { <Card {...props} /> }
-            />, section
+            <List header = "header" cards = {[]} />, section
         );
         ReactDOM.unmountComponentAtNode(section);
+    });
+    //snapshot test
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<List  header = "header" cards = {[]} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
